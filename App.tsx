b98c0e46baf56b83d31e7a64a25d124fa343c0b4
Nikolas <nikolasbitencourtt@gmail.com>
2023-03-16 14:04:30 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import CostumersScreen from "./src/screens/CostumersScreen";
+
+export default function App() {
+  return (
+    <View>
+      <CostumersScreen />
+    </View>
+  );
+}
---
+++ src/screens/CostumersScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CostumersScreen = () => {
+  return (
+    <View>
+      <Text>CostumersScreen</Text>
+    </View>
+  );
+};
+
+export default CostumersScreen;
